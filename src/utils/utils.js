78971304c2474f902cab01b79ecc13502277c785
Sviatoslav Mysachenko <svyatoslavik@gmail.com>
2016-12-25 21:37:09 +0200

    Small refactoring - replaced standart js function to ES 6 arrow funstion in utils.js

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -2,25 +2,21 @@ import _ from 'lodash';
 
 
 function getTasksWithCategories(tasksList) {
-  return tasksList.filter(function(task){
-    return task.categoryId;
-  });
+  return tasksList.filter(task => task.categoryId);
 }
 
 export function getCategoriesIdsWithUncompletedTasks(tasksList) {
-  return _.uniq(getTasksWithCategories(tasksList).filter(function(task){
-    return !task.done;
-  }).map(function(task){
-    return task.categoryId;
-  }));	
+  return _.uniq(getTasksWithCategories(tasksList)
+      .filter(task => !task.done)
+      .map(task => task.categoryId));
 }
 
 export function getCategoriesIdsWithTasks(tasksList) {
-  return _.uniq(getTasksWithCategories(tasksList).map(function(task){
-    return task.categoryId;
-  }))
+  return _.uniq(getTasksWithCategories(tasksList)
+      .map(task => task.categoryId));
 }
 
 export function getCategoriesIdsWithCompletedTasks(tasksList) {
-  return _.pullAll(getCategoriesIdsWithTasks(tasksList), getCategoriesIdsWithUncompletedTasks(tasksList));
+  return _.pullAll(getCategoriesIdsWithTasks(tasksList),
+      getCategoriesIdsWithUncompletedTasks(tasksList));
 }
